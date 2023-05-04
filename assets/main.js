68e9b90ab878e6b26eb80d05db5ad0cb23a8c651
brayanrodallega <thebrayan1a1@outlook.com>
2023-05-03 20:04:17 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCuvq-l94LVPqSZQnv8Lop4Q&part=snippet%2Cid&order=date&maxResults=50';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCuvq-l94LVPqSZQnv8Lop4Q&part=snippet%2Cid&order=date&maxResults=10';
 
 const content = null || document.getElementById('content');
 
@@ -20,19 +20,21 @@ async function getVideos(urlApi) {
     try {
         const videos = await getVideos(API);
         let view = `${videos.items.map(video => `
-            <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank"
+                <div class="group relative">
+                    <div
+                        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                        </h3>
+                    </div>
                 </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${video.snippet.title}
-                    </h3>
-                </div>
-            </div>
-        `).slice(1, 9).join('')}`;
+            </a>
+        `).splice(0, videos.items.length-1).join('')}`;
         content.innerHTML = view;
     } catch (error) {
         console.log(error);
